578457515c2f2357eb02c1242c8773cd3794a03a
ABDUL QHAYYUM ASSADI <abdul.qhayyum@matchmove.com>
2022-06-22 14:13:02 +0530

    auth route added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Navbar/>
         <Routes>
-          <Route path="/" element={<AuthShell/>}>
+          <Route path="/auth" element={<AuthShell/>}>
             <Route path="login" element={<Login/>}></Route>
             <Route path="signup" element={<SignUp/>}></Route>
           </Route>
