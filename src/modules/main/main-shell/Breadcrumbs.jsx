72246062209970c41a40258cbd82890ed7a01c89
abdulqhayum47 <37828235+abdulqhayum47@users.noreply.github.com>
2022-07-14 15:49:27 +0530

    Update src/modules/main/main-shell/Breadcrumbs.jsx

--- src/modules/main/main-shell/Breadcrumbs.jsx
+++ src/modules/main/main-shell/Breadcrumbs.jsx
@@ -19,7 +19,7 @@ const Breadcrumbs = () => {
           )}
             {pathnames.map((name, index) => {
             const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
-                const isLast = index === pathnames.length - 1;
+            const isLast = index === pathnames.length - 1;
                 return name !== 'main' ? (isLast ? (<Typography key={name}>{name}</Typography>) : (
                     <Link key={name} onClick={() => navigate(routeTo)}>{name}</Link>
               )
